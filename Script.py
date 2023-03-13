9105f8c74b7b15e9538b23322630a874a185c8ce
Matrix TG <108185896+matrixbotz0@users.noreply.github.com>
2023-03-13 09:02:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,18 +4,18 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/im_goutham_josh>GӨЦƬΉΛM ƧΣЯ😎✔</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/MLZ_BOTZ>𝐌𝐋𝐙 𝐁𝐨𝐭𝐳</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """Source Code - <a href=https://github.com/killadichandu/telegram-bot>Click Here</a>"""   
+    SOURCE_TXT = """Source Code - <a href=https://t.me/MLZ_BOTZ>Click Here</a>"""   
     
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and Kuttu BOT will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and  BOT will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
 1. kuttu bot should have admin privillage.
@@ -37,7 +37,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/im_goutham_josh)</code>
+<code>[Button Text](buttonurl:https://t.me/MLZ_BOTZ)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -64,7 +64,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Kuttu BOT
+these are the extra features of BOT
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
